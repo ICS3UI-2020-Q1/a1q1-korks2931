f34e40cb72e7ad1f802829b82187618774f43d50
korks2931 <korks2931@wrdsb.ca>
2020-09-14 15:13:30 +0000

    finished question A1Q1

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
  *
- * @author 
+ * @author Sevde Korkut 
  */
 public class Main {
 
@@ -9,7 +10,16 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // create a Scanner for user input
+    Scanner input = new Scanner (System.in);
+    System.out.println ("Please enter a temperature you would like to convert in degrees Celsius.");
+  // declare a variable to store Fahrenreit
+  
+  //declare a variable to store Celcius
+  int Celcius = input.nextInt ();
+  
+  int Fahrenreit = (Celcius*9)/5 + 32;
+
+  System.out.println (Celcius +"C is the same as " + Fahrenreit +  "F") ;
   }
 }
